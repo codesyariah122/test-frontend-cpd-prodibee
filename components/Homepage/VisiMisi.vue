ecf8dd8dd222413f101e0f46056880f19db759c1
codesyariah122 <pujiermanto@gmail.com>
2022-06-15 17:50:12 +0700

    update visi misi homepage

--- components/Homepage/VisiMisi.vue
+++ components/Homepage/VisiMisi.vue
@@ -3,7 +3,7 @@
     <mdb-container>
       <mdb-row class="justify-content-center visi-misi__header">
         <mdb-col lg="12">
-          <h1 :class="`${$device.isMobile ? 'text-white font-weight-bold mt-5 mb-5' : 'mt-5 mb-5'}`">Tentang Kami</h1>
+         
         </mdb-col>
       </mdb-row>
 
@@ -25,73 +25,73 @@
 </template>
 
 <script>
-import { FetchData } from "@/helpers";
-import BgImage from "~/assets/images/homepage/visi-misi-bg.jpg";
+  import { FetchData } from "@/helpers";
+  import BgImage from "~/assets/images/homepage/visi-misi-bg.jpg";
 
-export default {
-  data() {
-    return {
-      bg_image: BgImage,
-      context: {
-        visi: [],
-        misi: [],
-      },
-    };
-  },
-
-  beforeMount() {
-    this.ConfigApiUrl();
-  },
-
-  mounted() {
-    this.ContextData();
-  },
-
-  methods: {
-    ConfigApiUrl() {
-      const url = process.env.NUXT_ENV_API_URL;
-      this.$store.dispatch("config/storeConfigApiUrl", url);
+  export default {
+    data() {
+      return {
+        bg_image: BgImage,
+        context: {
+          visi: [],
+          misi: [],
+        },
+      };
     },
-    ContextData() {
-      FetchData(`${this.api_url}/web/home`)
+
+    beforeMount() {
+      this.ConfigApiUrl();
+    },
+
+    mounted() {
+      this.ContextData();
+    },
+
+    methods: {
+      ConfigApiUrl() {
+        const url = process.env.NUXT_ENV_API_URL;
+        this.$store.dispatch("config/storeConfigApiUrl", url);
+      },
+      ContextData() {
+        FetchData(`${this.api_url}/web/home`)
         .then((res) => {
           this.context.visi = res.visi_misi.visi;
           // console.log(res.visi_misi.misi.split(".").length)
           const split = res.visi_misi.misi.split(
             ".",
             res.visi_misi.misi.split(".").length - 1
-          );
+            );
           this.context.misi = [...split];
         })
         .catch((err) => console.error(err));
+      },
     },
-  },
 
-  computed: {
-    api_url() {
-      return this.$store.getters["config/ConfigApiUrl"];
+    computed: {
+      api_url() {
+        return this.$store.getters["config/ConfigApiUrl"];
+      },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
-.visi__misi .visi-misi__content h1 {
-  margin-left: 45px !important;
-  text-align: left;
-}
+  .visi__misi .visi-misi__content h1 {
+    margin-left: 45px !important;
+    text-align: left;
+  }
 
-.visi__misi .visi-misi__content p {
-  width: 100%;
-}
+  .visi__misi .visi-misi__content p {
+    width: 100%;
+  }
 
-.col-lg-8 {
-  flex: 0 0 100%;
-  max-width: 100%;
-}
+  .col-lg-8 {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
 
-.visi__misi:before {
-  /*background-color: #004899;*/
-  background-color: #048281;
-}
+  .visi__misi:before {
+    /*background-color: #004899;*/
+    background-color: #048281;
+  }
 </style>
